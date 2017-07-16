9afb68624416a4df214d184cfaadaa8d128503ad
Jason Ligon <jkligon26@users.noreply.github.com>
2017-07-16 16:10:05 -0700

    Update rename_Files.py

--- rename_Files.py
+++ rename_Files.py
@@ -2,15 +2,15 @@
 import os
 def rename_files():
     # get file names from a folder
-    fileList = os.listdir(r"C:\Users\jklig\Desktop\Folders\Python\Secret Message proj\Message")
+    fileList = os.listdir(r"...") #Insert directory
    # print(fileList)
     
     savedPath = os.getcwd()
     print("Current working directory is " + savedPath)
-    os.chdir(r"C:\Users\jklig\Desktop\Folders\Python\Secret Message proj\Message")
+    os.chdir(r"...") #Insert directory
     
     #for each file, rename the filename
-    table = str.maketrans(dict.fromkeys("0123456789"))
+    table = str.maketrans(dict.fromkeys("0123456789")) #removes numbers from the filename
     for fileName in fileList:
         print("Old name - " + fileName)
         print("New name - " + fileName.translate(table))
